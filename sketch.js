6303281e69a47eb67086a326ddd1d0f8927812b3
LokeshMachineni <114566736+LokeshMachineni@users.noreply.github.com>
2022-10-26 17:48:55 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var sea,ship;
-var seaImg,shipImg1;
+var seaImg,shipImg;
 
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg1 = loadAnimation("ship-1.png");
-shipImg1 = loadAnimation("ship-1.png");
-shipImg1 = loadAnimation("ship-1");
- shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
- shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  shipImg = loadAnimation("ship-1.png");
+  shipImg = loadAnimation("ship-1.png");
+  shipImg = loadAnimation("ship-1");
+  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
 }
@@ -25,7 +25,7 @@ function setup(){
 
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
+  ship.addAnimation("movingShip",shipImg);
   ship.scale =0.25;
   
 }
